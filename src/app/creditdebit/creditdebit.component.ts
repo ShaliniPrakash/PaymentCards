8f4008cf8382f2857c41e4664d1fa51b2b7d7171
ShaliniPrakash <57353900+ShaliniPrakash@users.noreply.github.com>
2019-12-16 18:47:57 +0530

    PaymentCards

---
+++ src/app/creditdebit/creditdebit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-creditdebit',
+  templateUrl: './creditdebit.component.html',
+  styleUrls: ['./creditdebit.component.css']
+})
+export class CreditdebitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
